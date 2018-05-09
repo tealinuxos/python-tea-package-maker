6c97e22387e11109be50972e305bdc3df57aa016
ciazhar <ciazhar.id@gmail.com>
2018-05-10 00:59:05 +0700

    add installation bash script

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "|---------------------------------|"
+echo "|----------Tea Maker GTK----------|"
+echo "|---------------------------------|"
+
+echo ""
+echo ""
+
+echo "defining var for execute"
+USR=./tea-maker-gtk/usr
+USR_ROOT=/usr
+
+echo ""
+echo ""
+
+echo "deploying"
+cp $USR $USR_ROOT
+
+echo ""
+echo ""
+echo "done~"
+
+echo "|---------------------------------|"
+echo "|------INSTALLATION COMPLETE0-----|"
+echo "|---------------------------------|"
